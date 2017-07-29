1e41d391c75b92822d6662dee6a4652ad6394ad4
cartond <cartond@email.sc.edu>
2017-07-29 18:06:46 -0400

    change to class to give input functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />
-};
+//functional component
+/*const SearchBar = () => {
+	return <input />;
+};*/
+
+//changed to class to give the input functionality.
+class SearchBar extends Component {
+	render() {
+		return <input onChange={event=>console.log(event.target.value)} />;
+	}
+}
 
 export default SearchBar;
