73fb915fbd594cb84aa9dbda8ae7e57fccd67ab3
cartond <cartond@email.sc.edu>
2017-08-15 00:21:34 -0400

    Load in videos, display first one

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,8 +7,21 @@ import React, { Component } from 'react';
 
 //changed to class to give the input functionality.
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''};
+	}
+
 	render() {
-		return <input onChange={event=>console.log(event.target.value)} />;
+		return (
+			<div>
+				<input 
+					value={this.state.term}
+					onChange={event=>this.setState({ term: event.target.value })} 
+				/>
+			</div>
+		)
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-//1-15
-import React from 'react';
+//2-29
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
 
 const API_KEY = 'AIzaSyCLKwgr81wdxAHnq0SF00MD5vXWu0zZ87E';
 
- // create a new comonenet that produces some html
-const App = () => {
-	return	(
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+
+		YTSearch({key: API_KEY, term: 'kanye west'}, (videos) => {
+			this.setState({ videos });
+			//same as this.setState({videos: videos});
+		});
+	}
+
+	render(){
+		return	(
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
- // take this component's generated html and
- // put it on the page
-
 ReactDom.render(<App />, document.querySelector('.container'));
-ReactDom.render(<SearchBar />, document.querySelector('.container'));
+// ReactDom.render(<SearchBar />, document.querySelector('.container'));
